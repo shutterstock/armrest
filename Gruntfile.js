65d221db5bd70d8d956e94c36a7f3f1e11f1c7a0
Ben Cochran <bcochran@shutterstock.com>
2013-10-09 11:24:36 -0400

    Upgrading "request" lib to 2.27.0 to solve a potential crashing problem when connections are deeply nested.
    Since "empty" requests now come back with body="" instead of body=undefined, updated callback handler to keep tests green.

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,8 @@ module.exports = function(grunt) {
 					'require': true,
 					'process': true,
 					'module': true,
-					'setTimeout': true
+					'setTimeout': true,
+					'console': true
 
 				}
 
