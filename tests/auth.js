b3fd6019c0e7e63fc226b64dbc1a4f6f40f1472c
Zubin Tiku <Rubikzube@Rubikzube.local>
2014-07-08 15:42:18 -0400

    Updated auth handling for objects vs strings

--- tests/auth.js
+++ tests/auth.js
@@ -1,8 +1,30 @@
 var server = require('./setup/server');
 var armrest = require('../lib');
 
-exports.authProperty = function(test) {
-	var client = armrest.client({ host: 'localhost:59903', auth: 'woo:hoo', logLevel: 'OFF' });
+exports.authPropertyString = function(test) {
+	var client = armrest.client({ 
+		host: 'localhost:59903', 
+		auth: 'woo:hoo',
+		logLevel: 'OFF'
+	});
+	client.get({
+		url: '/json',
+		success: function(data, response) {
+			test.equal(response.request.href, 'http://' + client.auth + '@' + client.hostname + ':' + client.port + '/json', 'authorized');
+			test.done();
+		}
+	});
+};
+
+exports.authPropertyObject = function(test) {
+	var client = armrest.client({ 
+		host: 'localhost:59903',
+		auth: { 
+			username: 'woo',
+			password: 'hoo'
+		}, 
+		logLevel: 'OFF'
+	});
 	client.get({
 		url: '/json',
 		success: function(data, response) {
@@ -13,7 +35,10 @@ exports.authProperty = function(test) {
 };
 
 exports.authBase = function(test) {
-	var client = armrest.client({ base: 'who:hoo@localhost:59903', logLevel: 'OFF' });
+	var client = armrest.client({ 
+		base: 'who:hoo@localhost:59903', 
+		logLevel: 'OFF'
+	});
 	client.get({
 		url: '/json',
 		success: function(data, response) {
