c5e304a1fefaab33041e6e1c962ecced51ce166f
Zubin Tiku <rubikzube@gmail.com>
2014-09-29 10:10:20 -0700

    Added support and tests for basePath

--- tests/factory.js
+++ tests/factory.js
@@ -1,3 +1,4 @@
+var server = require('./setup/server');
 var armrest = require('../lib');
 
 exports.simpleClient = function(test) {
@@ -15,17 +16,24 @@ exports.objectClient = function(test) {
 };
 
 exports.objectClientWithHostnamePort = function(test) {
-	var client = armrest.client({ hostname: 'localhost', port: 8675 });
+	var client = armrest.client({ hostname: 'localhost', port: 59903 });
 	test.equal(client.hostname, 'localhost');
-	test.equal(client.port, 8675);
+	test.equal(client.port, 59903);
 	test.done();
 };
 
-exports.objectClientWithBase= function(test) {
-	var client = armrest.client({ base: 'localhost:8675' });
+exports.objectClientWithBase = function(test) {
+	var client = armrest.client({ base: 'localhost:59903/multi/level', logLevel: 'OFF' });
 	test.equal(client.hostname, 'localhost');
-	test.equal(client.port, 8675);
-	test.done();
+	test.equal(client.port, 59903);
+	test.equal(client.basePath, '/multi/level');
+	client.get({
+		url: '/structure',
+		success: function(data, response) {
+			test.deepEqual(data, { level: 'level', structure: 'structure' }, 'Added the base path to the request');
+			test.done();
+		},
+	});
 };
 
 exports.constructor = function(test) {
@@ -35,5 +43,21 @@ exports.constructor = function(test) {
 		test.equal(e.constructor, TypeError, 'Cannot new up an instance');
 		test.equal(client, undefined, 'Cannot new up an instance');
 		test.done();
+	}
+};
+
+exports.setUp = function(callback) {
+	server.listen(59903, null, null, callback);
+};
+
+/*
+ * Trap the exception thrown if server.close is thrown on a server that is not
+ * running
+ */
+exports.tearDown = function(callback) {
+	try {
+		server.close(callback);
+	} catch (e) {
+		callback();
 	}
 };
