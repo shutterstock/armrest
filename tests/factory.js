87b674d9442d8b96948ef5a41f737128c5ad5ed5
Zubin Tiku <rubikzube@gmail.com>
2014-09-29 09:30:11 -0700

    Added test to the factory

--- tests/factory.js
+++ tests/factory.js
@@ -1,27 +1,34 @@
 var armrest = require('../lib');
 
-exports.simple = function(test) {
+exports.simpleClient = function(test) {
 	var client = armrest.client('localhost:59903');
 	test.equal(client.hostname, 'localhost');
 	test.equal(client.port, 59903);
 	test.done();
 };
 
-exports.object = function(test) {
+exports.objectClient = function(test) {
 	var client = armrest.client({ host: 'localhost:59903' });
 	test.equal(client.hostname, 'localhost');
 	test.equal(client.port, 59903);
 	test.done();
 };
 
-exports.objectWithHostnamePort = function(test) {
+exports.objectClientWithHostnamePort = function(test) {
 	var client = armrest.client({ hostname: 'localhost', port: 8675 });
 	test.equal(client.hostname, 'localhost');
 	test.equal(client.port, 8675);
 	test.done();
 };
 
-exports.constructArmrest = function(test) {
+exports.objectClientWithBase= function(test) {
+	var client = armrest.client({ base: 'localhost:8675' });
+	test.equal(client.hostname, 'localhost');
+	test.equal(client.port, 8675);
+	test.done();
+};
+
+exports.constructor = function(test) {
 	try {
 		var client = new armrest();
 	} catch (e) {
